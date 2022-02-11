efc2faf2d947c170e4553b9de120aec71ea5b490
Aurelien-Tallet <aurelien.jarte@gmail.com>
2022-02-11 15:56:22 +0100

    parser : sort words by syllables

--- parser/parser.js
+++ parser/parser.js
@@ -1,15 +1,5 @@
 const fs = require("fs");
 
-// let lines = [];
-// fs.readFile(process.argv[2], 'utf8' , (err, data) => {
-//    if (err) {
-//       console.error(err)
-//       return
-//    }
-//    lines = data.split('\n')
-//       .map(line => line.split(",")[0].split(" ")[0])
-// });
-
 let readableStream = fs.createReadStream(process.argv[2], {
   encoding: "utf-16le",
 });
@@ -19,13 +9,10 @@ readableStream.on("readable", () => {
   while ((chunk = readableStream.read()) !== null) {
     chunk
       .split("\n")
-      .map((line) =>
-        line
-          .split(",")[0]
-          .split(" ")[0]
-          .toLowerCase()
-          .replaceAll("\\", "")
-          .replaceAll(/\00/g, "")
+      .map(
+        (line) =>
+          line.split(",")[0].split(" ")[0].toLowerCase().replaceAll("\\", "")
+        // .replaceAll(/\00/g, "")
       )
       .forEach((word) =>
         res.add(word.normalize("NFD").replace(/\p{Diacritic}/gu, ""))
@@ -42,5 +29,219 @@ readableStream.on("end", () => {
       return;
     }
   });
-  console.log("finish");
+  sortWordBySyllabes();
 });
+
+const syllabes = [
+  "ai",
+  "an",
+  "ar",
+  "au",
+  "ce",
+  "ch",
+  "co",
+  "de",
+  "em",
+  "en",
+  "er",
+  "es",
+  "et",
+  "eu",
+  "ie",
+  "il",
+  "in",
+  "is",
+  "it",
+  "la",
+  "le",
+  "ma",
+  "me",
+  "ne",
+  "ns",
+  "nt",
+  "on",
+  "ou",
+  "pa",
+  "qu",
+  "ra",
+  "re",
+  "se",
+  "te",
+  "ti",
+  "tr",
+  "ue",
+  "un",
+  "ur",
+  "us",
+  "ve",
+  "le",
+  "en",
+  "es",
+  "de",
+  "re",
+  "ai",
+  "ou",
+  "nt",
+  "on",
+  "er",
+  "ur",
+  "an",
+  "it",
+  "te",
+  "et",
+  "me",
+  "la",
+  "is",
+  "qu",
+  "se",
+  "il",
+  "ue",
+  "us",
+  "eu",
+  "co",
+  "ra",
+  "ne",
+  "in",
+  "ve",
+  "pa",
+  "ma",
+  "au",
+  "ar",
+  "ns",
+  "ch",
+  "ie",
+  "ti",
+  "tr",
+  "ce",
+  "em",
+  "un",
+  " ain",
+  "ais",
+  "ait",
+  "ans",
+  "ant",
+  "ati",
+  "ava",
+  "ave",
+  "cha",
+  "che",
+  "com",
+  "con",
+  "dan",
+  "des",
+  "ell",
+  "eme",
+  "ent",
+  "est",
+  "éta",
+  "eur",
+  "eux",
+  "fai",
+  "ien",
+  "ion",
+  "ire",
+  "les",
+  "lle",
+  "lus",
+  "mai",
+  "men",
+  "mme",
+  "nte",
+  "omm",
+  "ont",
+  "our",
+  "ous",
+  "out",
+  "ouv",
+  "par",
+  "pas",
+  "plu",
+  "pou",
+  "que",
+  "res",
+  "son",
+  "sur",
+  "tai",
+  "tio",
+  "tou",
+  "tre",
+  "une",
+  "ure",
+  "ver",
+  "vou",
+  "ent",
+  "que",
+  "ait",
+  "les",
+  "lle",
+  "our",
+  "men",
+  "ais",
+  "est",
+  "tre",
+  "mai",
+  "ous",
+  "par",
+  "ant",
+  "ion",
+  "eme",
+  "tai",
+  "ans",
+  "pas",
+  "ell",
+  "vou",
+  "tou",
+  "pou",
+  "eur",
+  "ont",
+  "res",
+  "dan",
+  "une",
+  "éta",
+  "ien",
+  "sur",
+  "son",
+  "mme",
+  "tio",
+  "des",
+  "ire",
+  "com",
+  "omm",
+  "ver",
+  "con",
+  "che",
+  "ave",
+  "ain",
+  "ure",
+  "out",
+  "plu",
+  "cha",
+  "eux",
+  "ava",
+  "ouv",
+  "nte",
+  "lus",
+  "fai",
+  "ati",
+];
+function sortWordBySyllabes() {
+  const results = {};
+  syllabes.forEach(
+    (syl) =>
+      (results[syl] = Array.from(res).filter((word) =>
+        word.includes(syl.toLowerCase())
+      ))
+  );
+  writeJsonDict(results);
+}
+
+function writeJsonDict(data) {
+  const name = "output.json";
+  fs.writeFile(name, JSON.stringify(data), "utf-16le", (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  });
+  console.log("finish");
+}
